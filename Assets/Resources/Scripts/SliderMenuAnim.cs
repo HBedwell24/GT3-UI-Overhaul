ee11c8350c8ed23db6d3834da1c9ef04de848d1e
HBedwell24 <bedwellhb@gmail.com>
2022-02-13 13:22:55 -0500

    The sidebar now fades in and fades out when toggled.

--- Assets/Resources/Scripts/SliderMenuAnim.cs
+++ Assets/Resources/Scripts/SliderMenuAnim.cs
@@ -6,6 +6,20 @@ public class SliderMenuAnim : MonoBehaviour
 {
     public GameObject PanelMenu;
 
+    [SerializeField]
+    private CanvasGroup sideBar;
+
+    [SerializeField]
+    private CanvasGroup seperator;
+
+    public void Awake()
+    {
+        sideBar.alpha = 0;
+        sideBar.interactable = false;
+
+        seperator.alpha = 0;
+    }
+
     public void ShowHideMenu()
     {
         if(PanelMenu != null)
@@ -15,6 +29,40 @@ public class SliderMenuAnim : MonoBehaviour
             {
                 bool isOpen = animator.GetBool("show");
                 animator.SetBool("show", !isOpen);
+                StartCoroutine(FadeImage(isOpen));
+            }
+        }
+    }
+
+    IEnumerator FadeImage(bool isOpen)
+    {
+
+        // fade from opaque to transparent
+        if (isOpen)
+        {
+            // loop over 1 second backwards
+            for (float i = 1; i >= 0; i -= Time.deltaTime * 2)
+            {
+                // set color with i as alpha
+                sideBar.alpha = i;
+                seperator.alpha = i;
+
+                sideBar.interactable = false;
+                yield return null;
+            }
+        }
+        // fade from transparent to opaque
+        else
+        {
+            // loop over 1 second
+            for (float i = 0; i <= 1; i += Time.deltaTime * 2)
+            {
+                // set color with i as alpha
+                sideBar.alpha = i;
+                seperator.alpha = i;
+
+                sideBar.interactable = true;
+                yield return null;
             }
         }
     }
