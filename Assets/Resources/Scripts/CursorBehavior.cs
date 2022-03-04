70f6c7f7159e762878094ea6019ff4e453546832
HBedwell24 <bedwellhb@gmail.com>
2022-03-03 21:25:56 -0500

    Created a class to facilitate mouse movement on each of the screens along with button hover effects.

---
+++ Assets/Resources/Scripts/CursorBehavior.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+public class CursorBehavior : MonoBehaviour
+{
+    [SerializeField] RectTransform[] charBtn;
+    [SerializeField] RectTransform indicator;
+    [SerializeField] float moveDelay;
+
+    int indicatorPos;
+    float moveTimer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (moveTimer < moveDelay)
+        {
+            moveTimer += Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            if (moveTimer >= moveDelay)
+            {
+                if (indicatorPos < charBtn.Length - 1)
+                {
+                    AudioManager.instance.PlaySoundEffect("Cursor");
+                    indicatorPos++;
+                }
+                else
+                {
+                    indicatorPos = 0;
+                }
+                moveTimer = 0;
+            }
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            if (moveTimer >= moveDelay)
+            {
+                if (indicatorPos > 0)
+                {
+                    AudioManager.instance.PlaySoundEffect("Cursor");
+                    indicatorPos--;
+                }
+                else
+                {
+                    indicatorPos = charBtn.Length - 1;
+                }
+                moveTimer = 0;
+            }
+        }
+        indicator.localPosition = Vector3.Lerp(indicator.localPosition, charBtn[indicatorPos].localPosition, 0.2f);
+    }
+}
