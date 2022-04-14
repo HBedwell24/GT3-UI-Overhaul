b445c699c2b1944ed8deac9b3be922c22f465e93
HBedwell24 <bedwellhb@gmail.com>
2022-04-14 18:27:55 -0400

    Finished anchoring the various navigation bar components across the application.

--- Assets/Resources/Scripts/IdleDetector.cs
+++ Assets/Resources/Scripts/IdleDetector.cs
@@ -11,20 +11,17 @@ public class IdleDetector : MonoBehaviour
 
     void Update()
     {
-        if (Gamepad.current != null)
+        //Debug.Log(idleCounter);
+        if (Gamepad.current.allControls.Any(x => x is ButtonControl button && x.IsPressed() && !x.synthetic))
         {
-            //Debug.Log(idleCounter);
-            if (Gamepad.current.allControls.Any(x => x is ButtonControl button && x.IsPressed() && !x.synthetic))
+            idleCounter = 0.0f;
+        }
+        else if (idleCounter > 120.0f)
+        {
+            if (SceneManager.GetActiveScene().name != "Launch Screen")
             {
                 idleCounter = 0.0f;
-            }
-            else if (idleCounter > 120.0f)
-            {
-                if (SceneManager.GetActiveScene().name != "Launch Screen")
-                {
-                    idleCounter = 0.0f;
-                    loadingManager.GetComponent<LoadingManager>().LoadScene("Launch Screen,false");
-                }
+                loadingManager.GetComponent<LoadingManager>().LoadScene("Launch Screen,false");
             }
         }
         else
